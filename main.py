07579185e156c584924a52a3ffcd0b0a98a90395
Ricardo Oliveira <ricvenicius@gmail.com>
2026-04-28 17:46:42 -0300

    add uvicorn server startup in main.py

--- main.py
+++ main.py
@@ -62,3 +62,8 @@ async def ocr(file: UploadFile):
     )
 
     return {"filename": file.filename, "content": result}
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(api, host="0.0.0.0", port=8000)
