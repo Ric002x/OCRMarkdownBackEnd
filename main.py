c2f04f3fd35273640e4419c7eb540ee4d000b88e
Ricardo Oliveira <ricvenicius@gmail.com>
2026-04-28 21:36:14 -0300

    fix: update CORS settings to allow all origins in main.py

--- main.py
+++ main.py
@@ -11,14 +11,14 @@ api = FastAPI(
     title="File Conversion Application",
 )
 
-origins = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-]
+origins = ['*']
 
 api.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 
